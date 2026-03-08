77b0f3bab1f7ba6eedb71bd29e35d5911750c50a
Ömer ERTUĞ <omertug@gmail.com>
2026-03-09 00:08:48 +0300

    Create agency_performance.sql

---
+++ agency_performance.sql
@@ -0,0 +1,7 @@
+/*
+  PROJECT: Agesa Digital Transformation [cite: 24]
+  PURPOSE: To measure the average time difference between policy application and activation.
+  BUSINESS VALUE: Identifying bottlenecks in the digital issuance flow to increase efficiency by 50%.
+*/
+
+SELECT ... (sorgun buraya gelir)
